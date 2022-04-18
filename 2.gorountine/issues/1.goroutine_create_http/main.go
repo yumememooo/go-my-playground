ab3283f511e96cbedc9231b24e5d174586d1c6c5
yumememoooo <yumememooo@gmail.com>
2022-04-18 17:50:43 +0800

    Fix: update layout

--- 2.gorountine/issues/1.goroutine_create_http/main.go
+++ 2.gorountine/issues/1.goroutine_create_http/main.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
 	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
 )
 
-//go tool pprof http://localhost:8888/debug/pprof/heap?debug
-func demoFunc(i int) {
-	fmt.Println("Hello World!,I am done.", i)
-}
-
+//這個範例用來試著創建大量的go，然後透過gin的pprof接口監看效能
 func main() {
 	gin.SetMode(gin.ReleaseMode)
-
+	r := gin.Default()
+	v1 := r.Group("/v1")
+	{
+		v1.POST("/login", loginEndpoint)
+	}
 	engine := gin.New()
 	pprof.Register(engine) // pprof monitor
 	endpoint := fmt.Sprintf(":%d", 8888)
@@ -24,11 +26,10 @@ func main() {
 		}
 	}()
 
-	runTimes := 1048575 //給他一個大數目創建go
-	
-	for i := 0; i < runTimes; i++ {
-		go demoFunc(i)
-	}
 	fmt.Printf("end call")
 	select {} //避免退出
+}
+func loginEndpoint(c *gin.Context) {
+
+	c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
 }
