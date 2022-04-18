2e460383adb77703daff8a795eec2e290d8d7b6c
yumememoooo <yumememooo@gmail.com>
2022-04-18 20:35:53 +0800

    Add: 2.gorountine/issues  goroutine_create Case

--- 2.gorountine/issues/1.goroutine_create/main.go
+++ 2.gorountine/issues/1.goroutine_create/main.go
@@ -2,19 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"sync"
+	"time"
 
 	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-//go tool pprof http://localhost:8888/debug/pprof/heap?debug
 func demoFunc(i int) {
-	fmt.Println("Hello World!,I am done.", i)
+	//	fmt.Println("Hello World!,I am done.")  //如果直接印 竟然會造成堆積runtime.malg＠＠
+	//log.Println("Hello World!,I am done.")  //換個方法印 949990	goroutine 一樣堆積 201.58MB 73.29%   runtime.malg
+	//sugar.Debugf("debug message") //658392	goroutine // 152.56MB 23.67% 80.75%   152.56MB 23.67%  runtime.malg
+	// defer sugar.Sync() 不要這樣做 會變超級超級超級慢
+	// 如果sugur log level變成info 不印的話，一樣會造成塞住
+	// 但是會多了bufferpool
+	//265.25MB 56.07% 56.07%   265.25MB 56.07%  go.uber.org/zap/internal/bufferpool.init.func1
+  //102.54MB 21.67% 77.74%   102.54MB 21.67%  runtime.malg
+  // 如果main程式有做sugar.Sync() 還是卡在這邊
+	// 493.96MB 56.29% 56.29%   493.96MB 56.29%  go.uber.org/zap/internal/bufferpool.init.func1
+  // 194.58MB 22.17% 78.46%   194.58MB 22.17%  runtime.malg
+
+	//temp.Store(i, "") //不要印 只存到其他map 從頭到尾只有 5 goroutine 沒有堆積
+	//不做任何事 從頭到尾只有 5 goroutine 沒有堆積
+	//time.Sleep(1 * time.Second) //如果只處理一秒的情況？  //77710	goroutine 會堆積 180.57MB 77.51% 77.51%   180.57MB 77.51%  runtime.malg
+	time.Sleep(50 * time.Microsecond) //如果只處理50us的情況？ 856 goroutine 很快結束 4097.62kB 61.49% 61.49%  4097.62kB 61.49%  runtime.malg 有時沒堆積demoFunc
 }
+
+// 結論：所以不要在大量創建go的情境下去做印log的行為嗎？ 另外很快就結束的例子一樣不會同時花上太多的goＮum，也不會造成記憶體堆積
+
+var temp sync.Map
+var runGoNun = 0
 
 //這個範例用來試著創建大量的go，然後透過gin的pprof接口監看效能
 func main() {
 	gin.SetMode(gin.ReleaseMode)
+	InitLogger()
+	defer sugar.Sync()
 
 	engine := gin.New()
 	pprof.Register(engine) // pprof monitor
@@ -25,12 +52,60 @@ func main() {
 			fmt.Println(err)
 		}
 	}()
+	go runBigGo()
 
+	fmt.Printf("end call")
+
+	time.Sleep(120 * time.Second)
+	temp.Range(p)
+	log.Printf("runGoNun:%d", runGoNun) //runGoNun:1048575
+	select {}                           //避免退出
+}
+func p(key interface{}, value interface{}) bool {
+	runGoNun = runGoNun + 1
+	return true
+}
+
+func runBigGo() {
 	runTimes := 1048575 //給他一個大數目創建go
 
 	for i := 0; i < runTimes; i++ {
 		go demoFunc(i)
 	}
-	fmt.Printf("end call")
-	select {} //避免退出
+}
+
+// 418981	goroutine
+// 27	heap
+//結束：
+// 5	goroutine
+// 38	heap
+//go tool pprof http://localhost:8888/debug/pprof/heap
+// Showing top 10 nodes out of 32
+//       flat  flat%   sum%        cum   cum%
+//   177.07MB 52.06% 52.06%   177.07MB 52.06%  runtime.malg
+//    96.52MB 28.38% 80.43%    96.52MB 28.38%  fmt.glob..func1
+//    40.50MB 11.91% 92.34%    40.50MB 11.91%  internal/poll.runtime_Semacquire
+//    17.50MB  5.15% 97.49%    17.50MB  5.15%  fmt.(*buffer).write (inline)
+//     3.55MB  1.04% 98.53%     3.55MB  1.04%  runtime.allgadd
+//     2.50MB  0.73% 99.26%   157.02MB 46.16%  main.demoFunc
+//        2MB  0.59% 99.85%        2MB  0.59%  runtime.allocm
+//          0     0% 99.85%    17.50MB  5.15%  fmt.(*fmt).fmtInteger
+//          0     0% 99.85%    17.50MB  5.15%  fmt.(*fmt).pad
+//          0     0% 99.85%    17.50MB  5.15%  fmt.(*pp).doPrintln
+var sugar *zap.SugaredLogger
+
+func getEncoder() zapcore.Encoder {
+	return zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+}
+func getLogWriter() zapcore.WriteSyncer {
+	file, _ := os.Create("./test.log")
+	return zapcore.AddSync(file)
+}
+func InitLogger() {
+	writeSyncer := getLogWriter()
+	encoder := getEncoder()
+	core := zapcore.NewCore(encoder, writeSyncer, zapcore.DebugLevel)
+
+	logger := zap.New(core)
+	sugar = logger.Sugar()
 }
--- 2.gorountine/issues/1.goroutine_create_http/main.go
+++ 2.gorountine/issues/1.goroutine_create_http/main.go
@@ -11,12 +11,12 @@ import (
 //這個範例用來試著創建大量的go，然後透過gin的pprof接口監看效能
 func main() {
 	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
-	v1 := r.Group("/v1")
+	engine := gin.Default()
+	v1 := engine.Group("/v1")
 	{
-		v1.POST("/login", loginEndpoint)
+		v1.POST("/send", postData) //http://127.0.0.1:8888/v1/send
 	}
-	engine := gin.New()
+
 	pprof.Register(engine) // pprof monitor
 	endpoint := fmt.Sprintf(":%d", 8888)
 	go func() { //分析用
@@ -29,7 +29,93 @@ func main() {
 	fmt.Printf("end call")
 	select {} //避免退出
 }
-func loginEndpoint(c *gin.Context) {
-
+func postData(c *gin.Context) {
+	fmt.Println("Hello World!,I am done.")
 	c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
 }
+
+// 1. 先觀察啟動的量
+// http://localhost:8888/debug/pprof/
+// 5	goroutine
+// 8	heap
+
+// 2. 用baton 打ＰＯＳＴ http://127.0.0.1:8888/v1/send
+//go get -u github.com/americanexpress/baton  go1.17
+//baton -b test -m POST -u http://127.0.0.1:8888/v1/send -c 10 -t 10
+// =========================== Results ========================================
+// Total requests:                                150850
+// Time taken to complete requests:        10.000603959s
+// Requests per second:                            15082
+// Number of 2xx responses:                       150850
+// ===========================================================================
+// 過程中增長到 17 goroutine
+
+// 結束
+//== 5	goroutine
+//== 66	heap
+// Showing nodes accounting for 4610.84kB, 100% of 4610.84kB total
+// Showing top 10 nodes out of 37
+//       flat  flat%   sum%        cum   cum%
+//  2050.25kB 44.47% 44.47%  2050.25kB 44.47%  runtime.allocm
+//   512.31kB 11.11% 55.58%   512.31kB 11.11%  regexp/syntax.(*compiler).inst
+//   512.20kB 11.11% 66.69%   512.20kB 11.11%  runtime.malg
+//   512.04kB 11.11% 77.79%   512.04kB 11.11%  runtime.bgscavenge
+//   512.03kB 11.10% 88.90%   512.03kB 11.10%  net/textproto.(*Reader).ReadMIMEHeader
+//   512.01kB 11.10%   100%   512.01kB 11.10%  github.com/gin-gonic/gin.glob..func1
+
+/////===========
+// 再加大測一次  -c 200 Number of concurrent requests
+//test -m POST -u http://127.0.0.1:8888/v1/send -c 200 -t 10
+// // =========================== Results ========================================
+// // Total requests:                                164992
+// // Time taken to complete requests:        10.019925294s
+// // Requests per second:                            16466
+// // ===========================================================================
+// 206	goroutine
+// 78	heap
+// 結束： 看起來沒問題！！！！！！runtime.malg 沒增長
+//6	goroutine
+//81	heap
+// Showing top 10 nodes out of 34
+//       flat  flat%   sum%        cum   cum%
+//  4107.01kB 44.47% 44.47%  4107.01kB 44.47%  bufio.NewWriterSize (inline)
+//  2050.25kB 22.20% 66.67%  2050.25kB 22.20%  runtime.allocm
+//  1542.01kB 16.70% 83.36%  1542.01kB 16.70%  bufio.NewReaderSize (inline)
+//   512.31kB  5.55% 88.91%   512.31kB  5.55%  regexp/syntax.(*compiler).inst (inline)
+//   512.20kB  5.55% 94.46%   512.20kB  5.55%  runtime.malg
+//   512.04kB  5.54%   100%   512.04kB  5.54%  runtime.bgscavenge
+//          0     0%   100%  1542.01kB 16.70%  bufio.NewReader (inline)
+//          0     0%   100%   512.31kB  5.55%  github.com/go-playground/validator/v10.init
+//          0     0%   100%     2565kB 27.77%  net/http.(*conn).readRequest
+//          0     0%   100%  5649.02kB 61.16%  net/http.(*conn).serve
+
+//=========================================
+// 再加大 -c 20000
+// test -m POST -u http://127.0.0.1:8888/v1/send -c 20000 -t 10
+// =========================== Results ========================================
+// Total requests:                              34123782
+// Time taken to complete requests:        10.667939895s
+// Requests per second:                          3198723
+// 517 	goroutine
+// (pprof) top
+// Showing nodes accounting for 10257.11kB, 100% of 10257.11kB total
+// Showing top 10 nodes out of 49
+//       flat  flat%   sum%        cum   cum%
+//  3594.04kB 35.04% 35.04%  3594.04kB 35.04%  bufio.NewWriterSize
+//  2050.25kB 19.99% 55.03%  2050.25kB 19.99%  runtime.allocm
+//     1028kB 10.02% 65.05%     1028kB 10.02%  bufio.NewReaderSize (inline)
+//  1024.05kB  9.98% 75.03%  1024.05kB  9.98%  net/textproto.(*Reader).ReadMIMEHeader
+//   512.31kB  4.99% 80.03%   512.31kB  4.99%  regexp/syntax.(*compiler).inst
+//   512.20kB  4.99% 85.02%   512.20kB  4.99%  runtime.malg
+//   512.17kB  4.99% 90.02%   512.17kB  4.99%  github.com/gin-gonic/gin/render.writeContentType
+//   512.04kB  4.99% 95.01%   512.04kB  4.99%  runtime.bgscavenge
+
+// 結論：雖然go routine有加大 但是runtime.malg沒有增加
+// 但只要有印log就可能增加＠＠？
+// 2. 不管 -c 怎麼增加 goroutine 最多只到518
+// Showing top 10 nodes out of 31
+//       flat  flat%   sum%        cum   cum%
+//  3597.01kB 36.89% 36.89%  3597.01kB 36.89%  bufio.NewWriterSize
+//  1542.01kB 15.82% 52.71%  1542.01kB 15.82%  bufio.NewReaderSize (inline)
+//  1537.69kB 15.77% 68.48%  1537.69kB 15.77%  runtime.allocm
+//  1536.61kB 15.76% 84.24%  1536.61kB 15.76%  runtime.malg
